098aa21c963f13dda40e58c38fe6d4bcfabf1eb8
Ray <ray7us@gmail.com>
2024-01-27 12:44:01 +0530

    established connection with the sql lite db user: admin password admin123

--- authenticationproj/views.py
+++ authenticationproj/views.py
@@ -1,27 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-data = {
-  'movies':[
-
-    { 'id':75,
-     'name' :"good_man",
-     'release' : 2004
-    },
-     {'id':86,
-     'name' :"bat_man",
-     'release' : 2000
-    },
-     { 'id':75,
-     'name' :"wonder_man",
-     'release' : 2002
-    }
-
-    ]
-}
+from .models import Movie
 
 def authenticationproj(request):
-  return render(request,'templates/authenticationproj/home.html',data)
+  return HttpResponse("welocome")
 
 def home(request):
-  return HttpResponse("welocome")
+  data=Movie.objects.all() #  will generate a query to get all the data from the detabase 
+  return render(request,'authenticationproj/home.html',{'movies':data})
